aa3944fd03ea18b22c29a9aea44f97dd0c8b4294
bruguedes <bruguedes@gmail.com>
2021-09-28 23:09:17 -0500

    Initial commit

--- config/config.exs
+++ config/config.exs
@@ -1,33 +1,40 @@
 # This file is responsible for configuring your application
-# and its dependencies with the aid of the Config module.
+# and its dependencies with the aid of the Mix.Config module.
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
 
 # General application configuration
-import Config
+use Mix.Config
 
 config :rockelivery,
   ecto_repos: [Rockelivery.Repo]
 
+# Configuração para determinar que esse sera o adapter.
+config :rockelivery, Rockelivery.Users.Create, via_cep_adapter: Rockelivery.ViaCep.Client
+
+# Configuração para determinar que a Pk e FK serão do tipo UUID
+config :rockelivery, Rockelivery.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreing_key: [type: :binary_id]
+
+# Configuração para lib guardian
+config :rockelivery, RockeliveryWeb.Auth.Guardian,
+  issuer: "rockelivery",
+  secret_key: "XCvD3bE+o76K7cU2y5OpDANfQytPWA0gvs5m7bVdRhUQvPqo/oq68l0JtFTrFg2V"
+
+# Configuração para utilizar o pipeline do guardian para usar rota autenticada
+config :rockelivery, RockeliveryWeb.Auth.Pipeline,
+  module: RockeliveryWeb.Auth.Guardian,
+  error_handler: RockeliveryWeb.Auth.ErrorHandler
+
 # Configures the endpoint
 config :rockelivery, RockeliveryWeb.Endpoint,
   url: [host: "localhost"],
+  secret_key_base: "88tw6OjiQJ8+Z+995GgpphlQHXZgSw1bWSlInTyybUDpn5SX4dEXOpPwnc09cDS4",
   render_errors: [view: RockeliveryWeb.ErrorView, accepts: ~w(json), layout: false],
   pubsub_server: Rockelivery.PubSub,
-  live_view: [signing_salt: "p9hPJCMn"]
-
-# Configures the mailer
-#
-# By default it uses the "Local" adapter which stores the emails
-# locally. You can see the emails in your browser, at "/dev/mailbox".
-#
-# For production it's recommended to configure a different adapter
-# at the `config/runtime.exs`.
-config :rockelivery, Rockelivery.Mailer, adapter: Swoosh.Adapters.Local
-
-# Swoosh API client is needed for adapters other than SMTP.
-config :swoosh, :api_client, false
+  live_view: [signing_salt: "wcpB+xW5"]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -39,4 +46,4 @@ config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{config_env()}.exs"
+import_config "#{Mix.env()}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -1,4 +1,4 @@
-import Config
+use Mix.Config
 
 # Configure your database
 config :rockelivery, Rockelivery.Repo,
@@ -14,15 +14,12 @@ config :rockelivery, Rockelivery.Repo,
 #
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we use it
-# with esbuild to bundle .js and .css sources.
+# with webpack to recompile .js and .css sources.
 config :rockelivery, RockeliveryWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
-  check_origin: false,
-  code_reloader: true,
+  http: [port: 4000],
   debug_errors: true,
-  secret_key_base: "EB9q4BORftHf++ZpFUNaMwh4Mns0BNv6Clm9hNRmBaSLRfEu6nkwCzmufJ1X5Phs",
+  code_reloader: true,
+  check_origin: false,
   watchers: []
 
 # ## SSL Support
