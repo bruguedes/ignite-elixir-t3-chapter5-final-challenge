373175abf9f9a5fe57f33450c4c3a2707a12fae4
bruguedes <bruguedes@gmail.com>
2021-10-02 19:46:39 -0500

    chore: creating factory user and order

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,8 +1,14 @@
 defmodule Rockelivery.Factory do
   use ExMachina.Ecto, repo: Rockelivery.Repo
 
+  import Mox
+
   alias Ecto.UUID
+  alias Rockelivery.Items.Create, as: ItemCreate
+  alias Rockelivery.Items.Schemas.Item
+  alias Rockelivery.Users.Create, as: UserCreate
   alias Rockelivery.Users.Schemas.User
+  alias Rockelivery.ViaCep.ClientMock
 
   def user_params_factory do
     %{
@@ -26,6 +32,57 @@ defmodule Rockelivery.Factory do
       email: "bruguedes@gmail.com",
       password: "123123",
       name: "Bruno Guedes"
+    }
+  end
+
+  def create_user_factory do
+    expect(ClientMock, :get_cep_info, fn _cep ->
+      {:ok, "Avenida Epaminonda Jácome, Habitasa, Rio Branco/AC"}
+    end)
+
+    params = build(:user_params)
+    {:ok, user} = UserCreate.call(params)
+    user
+  end
+
+  def item_params_factory do
+    %{
+      "category" => "food",
+      "description" => "Banana 2",
+      "price" => "10.00",
+      "photo" => "/priv/photos/banaa.jpg"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      category: "food",
+      description: "Banana 2",
+      price: "10.00",
+      photo: "/priv/photos/banaa.jpg"
+    }
+  end
+
+  def create_item_factory do
+    params = build(:item_params)
+    {:ok, item} = ItemCreate.call(params)
+    item
+  end
+
+  def order_params_factory do
+    user = insert(:user)
+    item_1 = insert(:item)
+    item_2 = insert(:item)
+
+    %{
+      "address" => user.address,
+      "user_id" => user.id,
+      "comments" => "Com Pimenta",
+      "payment_method" => "credit_card",
+      "items" => [
+        %{"id" => item_1.id, "quantity" => 2},
+        %{"id" => item_2.id, "quantity" => 2}
+      ]
     }
   end
 end
