f7ce28e1bc298bd55c9103d767ce4f6540cd7922
bruguedes <bruguedes@gmail.com>
2021-10-09 21:43:56 -0500

    chore: alter table

--- priv/repo/migrations/20210806014758_create_orders_table.exs
+++ priv/repo/migrations/20210806014758_create_orders_table.exs
@@ -6,7 +6,7 @@ defmodule Rockelivery.Repo.Migrations.CreateOrdersTable do
       add :address, :string
       add :comments, :string
       add :payment_method, :payment_method
-      add :user_id, references(:users, type: :binary_id)
+      add :user_id, references(:users, type: :binary_id, on_delete: :nilify_all)
 
       timestamps()
     end
